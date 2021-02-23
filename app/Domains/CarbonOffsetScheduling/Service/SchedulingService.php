178dd47ccd1f0227600e9376810a707b06343b1e
Alex Eves <alex.eves@aiesoftware.com>
2021-02-23 17:38:46 +0000

    Green light on service test cases

--- app/Domains/CarbonOffsetScheduling/Service/SchedulingService.php
+++ app/Domains/CarbonOffsetScheduling/Service/SchedulingService.php
@@ -6,6 +6,24 @@ class SchedulingService
 {
     public function getSchedule(int $scheduleLengthInMonths, \DateTimeImmutable $subscriptionStartDate): array
     {
-        return [];
+        if ($scheduleLengthInMonths < 1) {
+            return [];
+        }
+
+        $scheduleDates = [];
+        for ($i = 1; $i <= $scheduleLengthInMonths; $i++) {
+            $scheduleDate = $subscriptionStartDate->add(new \DateInterval(sprintf('P%dM', $i)));
+
+            $baseDay = (int) $subscriptionStartDate->format('d');
+            $scheduleDay = (int) $scheduleDate->format('d');
+
+            if ($baseDay !== $scheduleDay) {
+                $scheduleDate = $scheduleDate->sub(new \DateInterval(sprintf('P%dD', $scheduleDay)));
+            }
+
+            $scheduleDates[] = $scheduleDate->format('Y-m-d');
+        }
+
+        return $scheduleDates;
     }
 }
