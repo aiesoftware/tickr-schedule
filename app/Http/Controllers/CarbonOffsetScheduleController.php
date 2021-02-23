d67de2700f38ed45a5a4176612d28662de41bfc6
Alex Eves <alex.eves@aiesoftware.com>
2021-02-23 18:19:20 +0000

    Validating scheduleInMonths request parameter

--- app/Http/Controllers/CarbonOffsetScheduleController.php
+++ app/Http/Controllers/CarbonOffsetScheduleController.php
@@ -5,11 +5,21 @@ namespace App\Http\Controllers;
 use App\Domains\CarbonOffsetScheduling\Command\GetScheduleCommand;
 use App\Domains\CarbonOffsetScheduling\Service\SchedulingService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CarbonOffsetScheduleController
 {
     public function getCarbonOffsetSchedule(Request $request, SchedulingService $service)
     {
+        $validator = Validator::make($request->all(), [
+            'scheduleInMonths' => ['required', 'integer', 'between:0,36'],
+            'subscriptionStartDate' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
         $command = new GetScheduleCommand(
             new \DateTimeImmutable($request->query->get('subscriptionStartDate')),
             (int) $request->query->get('scheduleInMonths')
--- tests/Feature/CarbonOffsetScheduleTest.php
+++ tests/Feature/CarbonOffsetScheduleTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Domains\CarbonOffsetScheduling\Command\GetScheduleCommand;
 use App\Domains\CarbonOffsetScheduling\Model\Schedule;
 use App\Domains\CarbonOffsetScheduling\Service\SchedulingService;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class CarbonOffsetScheduleTest extends TestCase
@@ -22,5 +23,44 @@ class CarbonOffsetScheduleTest extends TestCase
 
         $response = $this->get('/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=2');
         $this->assertEquals(json_encode(['2020-02-01', '2020-03-01']), $response->content());
+    }
+
+    /**
+     * @dataProvider innerBoundaryRequests
+     */
+    public function testInnerRequestBoundaries(string $url, string $outputMessage): void
+    {
+        $response = $this->get($url);
+        $this->assertEquals(Response::HTTP_OK, $response->status(), $outputMessage);
+    }
+
+    public function innerBoundaryRequests(): array
+    {
+        return [
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=0', 'scheduleInMonths accepts value of 0'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=36', 'scheduleInMonths accepts value of 36'],
+        ];
+    }
+
+    /**
+     * @dataProvider badRequests
+     */
+    public function testBadRequests(string $url, string $expectedError, string $outputMessage): void
+    {
+        $response = $this->get($url);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->status());
+        $this->assertStringContainsString($expectedError, $response->content(), $outputMessage);
+    }
+
+    public function badRequests(): array
+    {
+        return [
+            ['/carbon-offset-schedule?scheduleInMonths=1', 'The subscription start date field is required', 'subscriptionStartDate is required'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01', 'The schedule in months field is required', 'scheduleInMonths is required'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=four', 'The schedule in months must be an integer', 'scheduleInMonths requires an int'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=4a', 'The schedule in months must be an integer', 'scheduleInMonths requires an int'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=-1', 'The schedule in months must be between 0 and 36', 'scheduleInMonths has maximum allowed value of 36'],
+            ['/carbon-offset-schedule?subscriptionStartDate=2020-01-01&scheduleInMonths=37', 'The schedule in months must be between 0 and 36', 'scheduleInMonths has maximum allowed value of 36']
+        ];
     }
 }
